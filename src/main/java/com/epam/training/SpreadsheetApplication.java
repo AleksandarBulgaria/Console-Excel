fe0f36c02d563f25481cc99ad98b44aa480fcf1a
Aleksandar <aleksanderst@abv.bg>
2018-08-15 18:19:14 +0300

    Easy does it!

--- src/main/java/com/epam/training/SpreadsheetApplication.java
+++ src/main/java/com/epam/training/SpreadsheetApplication.java
@@ -1,22 +1,13 @@
 package com.epam.training;
 
+import com.epam.training.spreadsheet.Table;
+
 import java.util.Scanner;
 
 public class SpreadsheetApplication {
     private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Welcome to SpreadsheetApplication!");
-        String input = sc.nextLine();
-        System.out.println("not ready yet!");
-
-
-
-
-
-
-
-
-
-
+        Table t = new Table();
+        System.out.println(t);
     }
 }
