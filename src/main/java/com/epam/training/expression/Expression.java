36cc9c8915f5a28ce8b87a0d9565bc895bed8bf6
Visitor <n.nenkov@yahoo.com>
2018-08-15 12:19:04 +0300

    interface expression added

---
+++ src/main/java/com/epam/training/expression/Expression.java
@@ -0,0 +1,17 @@
+package com.epam.training.expression;
+
+public class Expression implements ExpressionInterface {
+
+    private Float value;
+    private ExpressionTree expTree;
+
+    @Override
+    public String getValueAsString() {
+        return null;
+    }
+
+    @Override
+    public float calculateExpression() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/epam/training/expression/ExpressionInterface.java
@@ -0,0 +1,6 @@
+package com.epam.training.expression;
+
+public interface ExpressionInterface {
+    public String getValueAsString();
+    public float calculateExpression();
+}
